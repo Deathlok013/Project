e9e7cabeb50c1b648471b5e2a07916781925a056
Alexandr <petrov.alex23@proton.me>
2023-09-17 17:55:24 +0300

    Initial Commit

---
+++ r234/r234App.swift
@@ -0,0 +1,17 @@
+//
+//  r234App.swift
+//  r234
+//
+//  Created by Macbook on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct r234App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
